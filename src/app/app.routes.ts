8e26e03fd6726377b80b30a1743c13102e528266
arunbabugsd <arun.sureshbabu@gosmartdining.com>
2024-04-16 16:04:10 +0530

    Initial setup with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AccountsComponent} from "./features/dashboard/accounts/accounts.component";
+import {DashboardComponent} from "./features/dashboard/dashboard/dashboard.component";
+import {ContactsComponent} from "./features/dashboard/contacts/contacts.component";
+import {DocumentsComponent} from "./features/dashboard/documents/documents.component";
+import {InvoicesComponent} from "./features/dashboard/invoices/invoices.component";
+import {OverviewComponent} from "./features/dashboard/overview/overview.component";
+import {ReportsComponent} from "./features/dashboard/reports/reports.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: DashboardComponent},
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      {path: 'accounts', component: AccountsComponent},
+      {path: 'contacts', component: ContactsComponent},
+      {path: 'documents', component: DocumentsComponent},
+      {path: 'invoices', component: InvoicesComponent},
+      {path: 'overview', component: OverviewComponent},
+      {path: 'reports', component: ReportsComponent}
+    ]
+  }
+];
