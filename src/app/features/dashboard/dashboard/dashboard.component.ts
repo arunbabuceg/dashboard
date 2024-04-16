058d6f267be0ffe25ba0a3b897ad3332e4f1f892
arunbabugsd <arun.sureshbabu@gosmartdining.com>
2024-04-16 18:21:07 +0530

    Updated upto routing

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {TopNavComponent} from "../../../top-nav/top-nav.component";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    TopNavComponent,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
